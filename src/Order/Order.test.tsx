82ca8060569b27caf4fabb3833847de8aa4eead6
Kirill Katashinsky <kir20-01@ya.ru>
2022-10-13 19:50:36 +0300

    Tests are written

--- src/Order/Order.test.tsx
+++ src/Order/Order.test.tsx
@@ -1,7 +1,87 @@
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, ShallowWrapper, shallow } from 'enzyme';
 import React from 'react';
 
+import { OrderComponent } from './Order';
+import type { Order } from '../data/fakeOrders';
+import { getDate } from '../utils/getDate';
+
+configure({ adapter: new Adapter() });
+jest.mock('../utils/getDate');
+
 describe('Order.tsx', () => {
-	it('some test', () => {
-		// test something here
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
+	it('should return null when order does not have shop', () => {
+		const order: Order = {
+			id: 123,
+			date: 1544356800000,
+			items: [
+				'Утиный пластмасса для показ новый год',
+				'Курица из нержавеющей стали, утка, гусь, голубь, питьевой фонтан',
+				'Новый стиль один розница яйцо для упаковки форма латекс',
+			]
+		};
+		const wrapper: ShallowWrapper = shallow(<OrderComponent order={order} />);
+
+		expect(wrapper.getElement()).toBeNull();
+	});
+
+	it('should return null when order does not have date', () => {
+		const order: Order = {
+			id: 123,
+			shop: 'Alihandro Express',
+			items: [
+				'Утиный пластмасса для показ новый год',
+				'Курица из нержавеющей стали, утка, гусь, голубь, питьевой фонтан',
+				'Новый стиль один розница яйцо для упаковки форма латекс',
+			]
+		};
+		const wrapper: ShallowWrapper = shallow(<OrderComponent order={order} />);
+
+		expect(wrapper.getElement()).toBeNull();
+	});
+
+	it('should return null when order does not have items', () => {
+		const order: Order = {
+			id: 123,
+			date: 1544356800000,
+			shop: 'Alihandro Express'
+		};
+		const wrapper: ShallowWrapper = shallow(<OrderComponent order={order} />);
+
+		expect(wrapper.getElement()).toMatchSnapshot();
+	});
+
+	it('should return null when items is empty', () => {
+		const order: Order = {
+			id: 123,
+			date: 1544356800000,
+			shop: 'Alihandro Express',
+			items: []
+		};
+		const wrapper: ShallowWrapper = shallow(<OrderComponent order={order} />);
+
+		expect(wrapper.getElement()).toMatchSnapshot();
+	});
+
+	it('should return template and call getDate when order has all keys', () => {
+		const order: Order = {
+			id: 123,
+			date: 1544356800000,
+			shop: 'Alihandro Express',
+			items: [
+				'Утиный пластмасса для показ новый год',
+				'Курица из нержавеющей стали, утка, гусь, голубь, питьевой фонтан',
+				'Новый стиль один розница яйцо для упаковки форма латекс',
+			]
+		};
+		const wrapper: ShallowWrapper = shallow(<OrderComponent order={order} />);
+
+		expect(wrapper).toMatchSnapshot();
+		expect(getDate).toHaveBeenCalledTimes(1);
+		expect(getDate).toHaveBeenCalledWith(order.date);
 	});
 });
--- src/utils/sortOrders.test.ts
+++ src/utils/sortOrders.test.ts
@@ -1,18 +1,69 @@
-import {sortByItemCount} from './sortOrders';
+import { sortOrders, getSortFunction, sortByItemCount, sortTypes, sortByDate } from './sortOrders';
+import type { Order } from '../data/fakeOrders';
 
-describe('sortByItemCount function', () => {
-	it('same items count', () => {
-		const order1 = {
-			items: ['item1', 'item2'],
-		};
+describe('sortOrders function', () => {
+	it('should not call sortFunction when array is empty', () => {
+		const orders: Array<Order> = [];
+		const sortFunction = jest.fn();
 
-		const order2 = {
-			items: ['1', '2'],
-		};
+		sortOrders(orders, sortFunction);
+		expect(sortFunction).not.toBeCalled();
+	});
 
-		const result = sortByItemCount(order1, order2);
 
-		expect(result).toBe(0);
+	it('should call sortFunction when array is not empty', () => {
+		const orders: Array<Order> = [
+			{ items: ['1', '2'], },
+			{ items: ['1', '2'], },
+		];
+		const sortFunction = jest.fn();
+
+		sortOrders(orders, sortFunction);
+		expect(sortFunction).toBeCalled();
 	});
 });
 
+describe('getSortFunction function', () => {
+	it('should not call function when sortType is not valid', () => {
+		expect(getSortFunction('NotValidSortType')).toBe(null);
+	});
+
+	it('should call function when sortType is valid', () => {
+		expect(getSortFunction(sortTypes.DATE)).toBe(sortByDate);
+		expect(getSortFunction(sortTypes.COUNT)).toBe(sortByItemCount);
+	});
+});
+
+type testTable = {
+	order1: Order;
+	order2: Order;
+	expected: number;
+};
+
+describe('sortByItemCount function', () => {
+	const table: Array<testTable> = [
+		{ order1: {}, order2: { items: ['1', '2'] }, expected: 0 },
+		{ order1: { items: ['1', '2'] }, order2: {}, expected: 0 },
+		{ order1: { items: ['1', '2'] }, order2: { items: ['1', '2'] }, expected: 0 },
+		{ order1: { items: ['1', '2'] }, order2: { items: ['1', '2', '3'] }, expected: -1 },
+		{ order1: { items: ['1', '2', '3'] }, order2: { items: ['1', '2'] }, expected: 1 },
+	];
+
+	it.each(table)('should return expected number', ({ order1, order2, expected }) => {
+		expect(sortByItemCount(order1, order2)).toBe(expected);
+	});
+});
+
+describe('sortByDate function', () => {
+	const table: Array<testTable> = [
+		{ order1: {}, order2: { date: 1 }, expected: 0 },
+		{ order1: { date: 1 }, order2: {}, expected: 0 },
+		{ order1: { date: 1 }, order2: { date: 1 }, expected: 0 },
+		{ order1: { date: 1 }, order2: { date: 2 }, expected: 1 },
+		{ order1: { date: 2 }, order2: { date: 1 }, expected: -1 },
+	];
+
+	it.each(table)('should return expected number', ({ order1, order2, expected }) => {
+		expect(sortByDate(order1, order2)).toBe(expected);
+	});
+});
