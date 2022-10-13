2163813ac5fbe29f7f7b2cd7436f96e22d7377ea
Kostich31 <k.v.kostinich@mail.ru>
2022-10-13 20:11:55 +0300

    add tests

--- src/Order/Order.test.tsx
+++ src/Order/Order.test.tsx
@@ -1,7 +1,46 @@
+jest.mock('../utils/getDate');
 import React from 'react';
+import { OrderComponent } from './Order';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { getDate } from '../utils/getDate';
+import { fakeOrders } from '../data/fakeOrders';
+
+configure({ adapter: new Adapter() });
 
 describe('Order.tsx', () => {
-	it('some test', () => {
-		// test something here
+	beforeEach(() => {
+		(getDate as jest.Mock).mockReturnValue('13 октября, чт, 2022 год');
+	});
+
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
+	it('Test null order', () => {
+		const order = null;
+		const wrapper = shallow(<OrderComponent order={order!} />)
+		expect(wrapper.getElement()).toBeNull();
+	});
+
+	it('Test unfilled order', () => {
+		const order = {
+			id: 1,
+			items: ['1', '2'],
+		};
+		const wrapper = shallow(<OrderComponent order={order} />)
+		expect(wrapper.getElement()).toBeNull();
+	});
+
+	it('Test empty order', () => {
+		const order = fakeOrders[0];
+		const wrapper = shallow(<OrderComponent order={order} />)
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('Test filled order', () => {
+		const order = fakeOrders[1];
+		const wrapper = shallow(<OrderComponent order={order} />)
+		expect(wrapper).toMatchSnapshot();
 	});
 });
--- src/utils/sortOrders.test.ts
+++ src/utils/sortOrders.test.ts
@@ -1,18 +1,59 @@
-import {sortByItemCount} from './sortOrders';
+import { sortByItemCount, sortTypes, sortByDate, getSortFunction } from './sortOrders';
+
+describe('getSortFunction function', () => {
+	test.each([
+		[sortTypes.COUNT, sortByItemCount],
+		[sortTypes.DATE, sortByDate],
+		['null', null]
+	])('Returns correct function', ((sortType, expected) => {
+		expect(getSortFunction(sortType)).toBe(expected);
+	}));
+});
 
 describe('sortByItemCount function', () => {
-	it('same items count', () => {
-		const order1 = {
-			items: ['item1', 'item2'],
-		};
+	test.each([
+		{ order1: { items: ['1', '2', '3', '4', '5', '6'] }, order2: { items: ['1', '2', '3', '4', '5'] }, expected: 1 },
+		{ order1: { items: ['1', '2', '3', '4'] }, order2: { items: ['1', '2', '3', '4', '5'] }, expected: -1 },
+		{ order1: { items: ['1', '2', '3', '4', '5'] }, order2: { items: ['1', '2', '3', '4', '5'] }, expected: 0 },
+		{ order1: { items: [] }, order2: { items: ['1', '2', '3', '4', '5'] }, expected: -1 },
+		{ order1: { items: ['1', '2', '3', '4', '5'] }, order2: { items: [] }, expected: 1 },
+		{ order1: { items: undefined }, order2: { items: ['1', '2', '3', '4', '5'] }, expected: 0 },
+		{ order1: { items: ['1', '2', '3', '4', '5'] }, order2: { items: undefined }, expected: 0 },
+		{ order1: { items: ['1', '2', '3', '4', '5'] }, order2: {}, expected: 0 },
+		{ order1: undefined, order2: { items: ['1', '2', '3', '4', '5'] }, expected: 0 },
+		{ order1: { items: ['1', '2', '3', '4', '5'] }, order2: undefined, expected: 0 },
 
-		const order2 = {
-			items: ['1', '2'],
-		};
-
-		const result = sortByItemCount(order1, order2);
-
-		expect(result).toBe(0);
+	])('Testing orders %s', ({ order1, order2, expected }) => {
+		expect(sortByItemCount(order1!, order2!)).toBe(expected);
 	});
 });
 
+describe('sortByItemDate function', () => {
+	const dateNow = Date.now();
+	test.each([
+		{ order1: { date: dateNow }, order2: { date: dateNow + 1 }, expected: 1 },
+		{ order1: { date: dateNow + 1 }, order2: { date: dateNow }, expected: -1 },
+		{ order1: { date: dateNow }, order2: { date: dateNow }, expected: 0 },
+		{ order1: { date: 0 }, order2: { date: dateNow }, expected: 0 },
+		{ order1: { date: dateNow }, order2: { date: 0 }, expected: 0 },
+		{ order1: undefined, order2: { date: dateNow }, expected: 0 },
+		{ order1: { date: dateNow }, order2: undefined, expected: 0 }
+	])('Testing orders %s', ({ order1, order2, expected }) => {
+		expect(sortByDate(order1!, order2!)).toBe(expected);
+	});
+});
+
+describe('sortByItemDate function', () => {
+	const dateNow = Date.now();
+	test.each([
+		{ order1: { date: dateNow }, order2: { date: dateNow + 1 }, expected: 1 },
+		{ order1: { date: dateNow + 1 }, order2: { date: dateNow }, expected: -1 },
+		{ order1: { date: dateNow }, order2: { date: dateNow }, expected: 0 },
+		{ order1: { date: 0 }, order2: { date: dateNow }, expected: 0 },
+		{ order1: { date: dateNow }, order2: { date: 0 }, expected: 0 },
+		{ order1: undefined, order2: { date: dateNow }, expected: 0 },
+		{ order1: { date: dateNow }, order2: undefined, expected: 0 }
+	])('Testing orders %s', ({ order1, order2, expected }) => {
+		expect(sortByDate(order1!, order2!)).toBe(expected);
+	});
+});
