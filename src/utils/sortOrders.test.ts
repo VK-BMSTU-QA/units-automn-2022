92c570a7213419fcb83699159e31918892b704f7
Kislv <vityakiselev96@gmail.com>
2022-10-13 20:25:31 +0300

    add: fix sort function test

--- src/utils/sortOrders.test.ts
+++ src/utils/sortOrders.test.ts
@@ -104,50 +104,20 @@ describe('sortOrders function', () => {
 		expect(orders).toBe(undefined);
 	});
 
-	const fakeOrdersSorted: Order[] = [
-		{
-			id: 100,
-			date: 1588359900000,
-			shop: 'Сбереги Мега Маркер',
-			items: [],
-		},
-		{
-			id: 126,
-			date: 1652585550000,
-			shop: 'Эльдоградо',
-			items: [
-				'Ноутбук Apple MacBook Air 13.3" (MQD32RU/A)',
-				'Игровая приставка Sony PlayStation 4 Pro 1TB Black (CUH-7208B)',
-			]
-		},
-		{
-			id: 123,
-			date: 1544356800000,
-			shop: 'Alihandro Express',
-			items: [
-				'Утиный пластмасса для показ новый год',
-				'Курица из нержавеющей стали, утка, гусь, голубь, питьевой фонтан',
-				'Новый стиль один розница яйцо для упаковки форма латекс',
-			]
-		},
-		{
-			id: 124,
-			date: 1652481120000,
-			shop: 'Lamodник.ru',
-			items: [
-				'Жакет - BOREAL5',
-				'Miss Gabby Костюм',
-				'Ostin перчатки мужские',
-				'Zara худи роз.',
-			]
-		},
+	it('empty orders', () => {
 
-	];
-
-	test.each([
-		[fakeOrders, sortByItemCount, fakeOrdersSorted],])('valid orders', (orders, sortFunction, expected) => {
+		const orders = undefined;
+		const sortFunction = sortByItemCount;
 		const result = sortOrders(orders, sortFunction);
-		expect(orders).toStrictEqual(expected);
+
+		expect(orders).toBe(undefined);
+	});
+
+	let sortF = jest.fn(); 
+	test.each([
+		[fakeOrders, sortF]])('valid orders', (orders, sortFunction) => {
+			const result = sortOrders(orders, sortFunction);
+		expect(sortF).toBeCalled();
 	});
 });
 
