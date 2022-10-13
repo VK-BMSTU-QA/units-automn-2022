88a4df768d80b1deceebbeab85923a62d7f629ee
Natali-Skv <nat-s.skv@mail.ru>
2022-10-13 21:16:27 +0300

    units ready

--- src/Order/Order.test.tsx
+++ src/Order/Order.test.tsx
@@ -1,7 +1,68 @@
 import React from 'react';
+import { getDate } from '../utils/getDate';
+import { Order } from '../data/fakeOrders';
+jest.mock('../utils/getDate');
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { OrderComponent } from './Order';
+
+configure({ adapter: new Adapter() });
 
 describe('Order.tsx', () => {
-	it('some test', () => {
-		// test something here
+	beforeEach(() => {
+		(getDate as jest.Mock).mockReturnValue('14 мая, сб, 2022 год');
+	});
+
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
+	it('should render regular order', () => {
+		const order: Order = {
+			id: 123,
+			date: 1544356800000,
+			shop: 'Alihandro Express',
+			items: [
+				'Утиный пластмасса для показ новый год',
+				'Курица из нержавеющей стали, утка, гусь, голубь, питьевой фонтан',
+				'Новый стиль один розница яйцо для упаковки форма латекс',
+			]
+		};
+		const wrapper = shallow(<OrderComponent order={order} />);
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('should render order with empty items', () => {
+		const order: Order = {
+			id: 123,
+			date: 1544356800000,
+			shop: 'Alihandro Express',
+			items: []
+		};
+		const wrapper = shallow(<OrderComponent order={order} />);
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('should call getDate() while rendering regular order', () => {
+		const order: Order = {
+			id: 123,
+			date: 1544356800000,
+			shop: 'Alihandro Express',
+			items: [
+				'Утиный пластмасса для показ новый год',
+			]
+		};
+		const wrapper = shallow(<OrderComponent order={order} />);
+		expect(getDate).toBeCalled();
+	});
+
+	test.each([
+		{ id: undefined, date: 0, shop: undefined, items: [] },
+		{ id: 0, date: undefined, shop: 'TestShop', items: [] },
+		{ id: 0, date: 0, shop: undefined, items: [] },
+		{ id: 0, date: 0, shop: 'TestShop', items: undefined },
+	] as Order[])('should not render order when some field is undefined', (order) => {
+		const orderComponent = OrderComponent({ order: order });
+		expect(orderComponent).toBeNull();
 	});
 });
--- src/utils/sortOrders.test.ts
+++ src/utils/sortOrders.test.ts
@@ -1,18 +1,111 @@
-import {sortByItemCount} from './sortOrders';
+import { sortByItemCount, sortByDate, getSortFunction, sortTypes, sortOrders } from './sortOrders';
+import { Order } from '../data/fakeOrders';
 
 describe('sortByItemCount function', () => {
-	it('same items count', () => {
-		const order1 = {
-			items: ['item1', 'item2'],
-		};
+	test.each([
+		[{ items: ['item-1-1', 'item-1-2'] }, { items: ['item-2-1', 'item-2-2'] }, 0],
+		[{ items: ['item-1'] }, { items: ['item-2'] }, 0],
+		[{ items: [] }, { items: [] }, 0],
+	])('should return 0 when the number of items is same', (order1, order2, expectedRes) => {
+		expect(sortByItemCount(order1, order2)).toBe(expectedRes);
+	});
 
-		const order2 = {
-			items: ['1', '2'],
-		};
+	test.each([
+		[{ items: [] }, { items: ['item-1-1'] }],
+		[{ items: ['item-1-1'] }, { items: ['item-2-1', 'item-2-2'] }],
+		[{ items: ['item-1-1', 'item-1-2'] }, { items: ['item-2-1', 'item-2-2', 'item-2-3'] }],
+	])('should return (-1) when the number of items in the first order is less than in the second order', (order1, order2) => {
+		expect(sortByItemCount(order1, order2)).toBe(-1);
+	});
 
-		const result = sortByItemCount(order1, order2);
+	test.each([
+		[{ items: ['item-1-1'] }, { items: [] }],
+		[{ items: ['item-1-1', 'item-1-2'] }, { items: ['item-2-1'] }],
+		[{ items: ['item-1-1', 'item-1-2', 'item-1-3'] }, { items: ['item-2-1', 'item-2-2'] }],
+	])('should return (1) when the number of items in the first order is greater than in the second order', (order1, order2) => {
+		expect(sortByItemCount(order1, order2)).toBe(1);
+	});
 
-		expect(result).toBe(0);
+	test.each([
+		[{}, { items: ['item-2-1'] }],
+		[{ items: ['item-1-1'] }, {}],
+		[undefined, { items: ['item-2-1'] }],
+		[{ items: ['item-1-1'] }, undefined],
+	])('should return 0 when some of order is empty structure or undefined', (order1, order2) => {
+		expect(sortByItemCount(order1, order2)).toBe(0);
 	});
 });
 
+describe('sortByDate function', () => {
+	it('should return (1) when date of the first order less then of the second', () => {
+		const order1: Order = {
+			date: 1,
+		};
+		const order2: Order = {
+			date: 2,
+		};
+		expect(sortByDate(order1, order2)).toBe(1);
+	});
+
+	it('should return (-1) when date of the first order greater then of the second', () => {
+		const order1: Order = {
+			date: 2,
+		};
+		const order2: Order = {
+			date: 1,
+		};
+		expect(sortByDate(order1, order2)).toBe(-1);
+	});
+
+	it('should return 0 when dates equal', () => {
+		const order1: Order = {
+			date: 1,
+		};
+		const order2: Order = {
+			date: 1,
+		};
+		expect(sortByDate(order1, order2)).toBe(0);
+	});
+
+	test.each([
+		[{}, { date: 1 }],
+		[{ date: 1 }, {}],
+		[undefined, { date: 1 }],
+		[{ date: 1 }, undefined],
+	])('should return 0 when some of order is empty structure or undefined', (order1, order2) => {
+		expect(sortByDate(order1, order2)).toBe(0);
+	});
+});
+
+describe('getSortFunction function', () => {
+	it('sort by count', () => {
+		expect(getSortFunction(sortTypes.COUNT)).toBe(sortByItemCount);
+	});
+	it('sort by date', () => {
+		expect(getSortFunction(sortTypes.DATE)).toBe(sortByDate);
+	});
+	it('not date or count sort', () => {
+		expect(getSortFunction('notCountAndNoteDate')).toBeNull();
+	});
+});
+
+describe('sortOrders function', () => {
+	it('should call sortFunction when orders is not empty', () => {
+		const orders: Order[] = [{},{}];
+		const mockFunc = jest.fn();
+		expect(() => {
+			sortOrders(orders, mockFunc);
+		}).not.toThrow();
+		expect(mockFunc).toBeCalled();
+	});
+
+	it('should not call sortFunction when orders is empty or undefiened', () => {
+		const orders: Order[] = [];
+		const mockFunc = jest.fn();
+		expect(() => {
+			sortOrders(orders, mockFunc);
+		}).not.toThrow();
+		expect(orders).toEqual([] as Order[]);
+		expect(mockFunc).not.toBeCalled();
+	});
+});
