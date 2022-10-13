05f538071f1fb34eacbe41171e7e71e40bc40bd3
nezhibitskiy <nezhibitskiy@yandex.ru>
2022-10-13 20:07:37 +0300

    added unit tests

--- src/Order/Order.test.tsx
+++ src/Order/Order.test.tsx
@@ -1,7 +1,59 @@
 import React from 'react';
+import {shallow, configure} from 'enzyme';
+import {OrderComponent} from './Order';
+import Adapter from 'enzyme-adapter-react-16';
 
-describe('Order.tsx', () => {
-	it('some test', () => {
-		// test something here
+jest.mock('../utils/getDate');
+import {getDate} from '../utils/getDate';
+configure({ adapter: new Adapter() });
+
+describe('OrderComponent functions', () => {
+
+	beforeEach(() => {
+		getDate.mockReturnValue('test date');
 	});
+	afterAll(() => {
+		jest.clearAllMocks();
+	});
+
+	it('empty order', () => {
+		const testOrder = {};
+		const wrapper = shallow(<OrderComponent order={testOrder}/>);
+		expect(wrapper.getElement()).toBeNull();
+	});
+
+	test.each([
+		{testOrder: {id: 1, shop: 'test'}},
+		{testOrder: {id: 1, date: 13102022}},
+	])('order.shop is empty or order.date is empty', ({testOrder}) => {
+		const wrapper = shallow(<OrderComponent order={testOrder}/>);
+		expect(wrapper.getElement()).toBeNull();
+	});
+
+	it('!order.items', () => {
+		const testOrder = {
+			id: 1,
+			date: 13102022,
+			shop: 'test',
+		};
+		const wrapper = shallow(<OrderComponent order={testOrder}/>);
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('positive test', () => {
+		const testOrder = {
+			id: 1,
+			date: 13102022,
+			shop: 'test',
+			items: [
+				'test',
+				'test',
+				'test',
+				'test',
+			]
+		};
+		const wrapper = shallow(<OrderComponent order={testOrder}/>);
+		expect(wrapper).toMatchSnapshot();
+	});
+
 });
