cac4089bdd6fbd57dee11e763c1c2265db9aad21
papazloynt <cool.chastikov53@gmail.com>
2022-10-13 20:44:17 +0300

    feat: add test & coverage > 90%

--- src/Order/Order.test.tsx
+++ src/Order/Order.test.tsx
@@ -1,7 +1,60 @@
+jest.mock('../utils/getDate');
+
 import React from 'react';
+import {getDate} from '../utils/getDate';
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { OrderComponent } from './Order';
+
+configure({ adapter: new Adapter() });
 
 describe('Order.tsx', () => {
-	it('some test', () => {
-		// test something here
+	beforeEach(() => {
+		getDate.mockReturnValue('15 мая, ср, 2100 год');
+	});
+
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
+	it('empty order', () => {
+		const order = {};
+
+		const wrapper = shallow(<OrderComponent order={order}/>);
+		expect(wrapper.getElement()).toBeNull();
+	});
+
+
+	test.each([
+		{order: {id: 1, date: 1588359900000}},
+		{order: {id: 1, shop: 'shop'}},
+	])('Check for empty', ({order}) => {
+		const wrapper = shallow(<OrderComponent order={order}/>);
+		expect(wrapper.getElement()).toBeNull();
+	});
+
+	it('without orders items', () => {
+		const order = {
+			id: 1,
+			date: 1588359900000,
+			shop: 'shop',
+		};
+		const wrapper = shallow(<OrderComponent order={order}/>);
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('valid work', () => {
+		const order = {
+			id: 1,
+			date: 1588359900000,
+			shop: 'shop',
+			items: [
+				'item1',
+				'item2',
+				'item3',
+			]
+		};
+		const wrapper = shallow(<OrderComponent order={order}/>);
+		expect(wrapper).toMatchSnapshot();
 	});
 });
--- src/utils/sortOrders.test.ts
+++ src/utils/sortOrders.test.ts
@@ -1,6 +1,40 @@
-import {sortByItemCount} from './sortOrders';
+import {getSortFunction, sortByDate, sortByItemCount} from './sortOrders';
+
+describe('getSortFunction function', () => {
+	it('get sortByDate', () => {
+		const sortType = 'date';
+
+		const result = getSortFunction(sortType);
+
+		expect(result).toBe(sortByDate);
+	});
+	it('get sortByItemCount', () => {
+		const sortType = 'count';
+
+		const result = getSortFunction(sortType);
+
+		expect(result).toBe(sortByItemCount);
+	});
+	it('get null', () => {
+		const sortType = 'undefined';
+
+		const result = getSortFunction(sortType);
+
+		expect(result).toBe(null);
+	});
+});
+
 
 describe('sortByItemCount function', () => {
+	it('there is no items', () => {
+		const order1 = {};
+
+		const order2 = {};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(0);
+	});
 	it('same items count', () => {
 		const order1 = {
 			items: ['item1', 'item2'],
@@ -14,5 +48,81 @@ describe('sortByItemCount function', () => {
 
 		expect(result).toBe(0);
 	});
+	it('first item count more than second items count', () => {
+		const order1 = {
+			items: ['item1', 'item2', 'item3'],
+		};
+
+		const order2 = {
+			items: ['1', '2'],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(1);
+	});
+	it('second item count more than first items count', () => {
+		const order1 = {
+			items: ['item1', 'item2'],
+		};
+
+		const order2 = {
+			items: ['1', '2', '3'],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(-1);
+	});
 });
 
+describe('sortByDate function', () => {
+	it('there is no date', () => {
+		const order1 = {};
+
+		const order2 = {};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+	it('same items date', () => {
+		const order1 = {
+			date: 1588359900000,
+		};
+
+		const order2 = {
+			date: 1588359900000,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+	it('first was early than second', () => {
+		const order1 = {
+			date: 1588359900000,
+		};
+
+		const order2 = {
+			date: 1588359900000 + 1588359900000,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(1);
+	});
+	it('second was early than first', () => {
+		const order1 = {
+			date: 1588359900000 + 1588359900000,
+		};
+
+		const order2 = {
+			date: 1588359900000,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(-1);
+	});
+});
